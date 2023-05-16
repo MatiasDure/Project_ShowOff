4c5cc5c04ad191c879e83ad169ed34a4f68f7832
OrestisSMD <corestis@yahoo.com>
2023-05-16 13:18:44 +0200

    Setting up quests

---
+++ Assets/Scripts/LevelQuest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LevelQuest : MonoBehaviour
+{
+    public virtual void StartQuest()
+    {
+    }
+
+    public virtual void CompleteQuest()
+    {
+    }
+}
