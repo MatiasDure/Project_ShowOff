354cc92339f1551b1dd9be3bf5cd33a20e7ca9b0
Nico200209 <espallemann@gmail.com>
2023-06-01 14:58:57 +0200

    Sound Implementation Fear Level
    Tree + Lamp Post + Torches

--- Assets/Scripts/PathTorch.cs
+++ Assets/Scripts/PathTorch.cs
@@ -9,14 +9,38 @@ public class PathTorch : ToggleObject
     public event Action<PathTorch> OnTorchDisable;
 
     InteractionInformation info; // Dummy info
+
+    //Nico 
+    private static int torchCount = 0;
+
     protected override void Interact(InteractionInformation pInfo)
     {
         info = pInfo;
         OnTorchEnable?.Invoke(this);
+
+        //Nico
+        
+        
+
+       
     }
 
     public void EnableTorch()
     {
         base.Interact(info);
+        //Nico
+        torchCount++;
+        float pitch = 1.0f + (torchCount * 0.1f); // Adjust the pitch increment as needed
+        float pitchKey = 0.5f + ((torchCount - 1) * 2.0f) / 12f;
+        AudioManager.instance.PlayWithPitch("LightUp", 1);
+        if(pitchKey <= 3)
+        {
+            AudioManager.instance.PlayWithPitch("KeyNote", pitchKey);
+        }
+        else
+        {
+            AudioManager.instance.PlayWithPitch("FinalNote", 1);
+        }
+        
     }
 }
---
+++ Assets/SelfmadePKG/FearLvL/AudioManager.cs
@@ -0,0 +1,89 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.priority = s.priority;
+        }
+    }
+    /**
+    void Start()
+    {
+        Play("Theme");
+    }/**/
+
+    public void Play (string name)
+    {
+        Sound s =  Array.Find(sounds, sound => sound.name == name); // To play sound in specific point:
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        else
+        {
+            Debug.Log("Playing: " + name);
+        }
+        s.source.Play();                                           // FindObjectOfType<AudioManager>().Play("Sound_Name");
+    }
+
+    //Nico
+    public void PlayWithPitch(string name, float pitch)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            Debug.Log("Playing: " + name + " with pitch: " + pitch);
+        }
+        s.source.pitch = pitch;
+        s.source.Play();
+    }
+
+    public float GetClipLength(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            return s.Length;
+        }
+        else
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return 0f;
+        }
+    }
+}
